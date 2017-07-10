cc4e288920b17ecd75d82d0a120421a4d7cd8fcf
John Crisostomo <johncrisostomo@protonmail.ch>
2017-07-10 19:56:27 +0800

    sets up basic routes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
   authButton = () => {
@@ -8,21 +8,19 @@ class Header extends Component {
 
   render() {
     return (
-      <Router>
-        <nav className="navbar navbar-light">
-          <ul className="nav navbar-nav">
-            <li className="nav-item">
-              <Link to="/">Home</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/resources">Resources</Link>
-            </li>
-            <li className="nav-item">
-              {this.authButton()}
-            </li>
-          </ul>
-        </nav>
-      </Router>
+      <nav className="navbar navbar-light">
+        <ul className="nav navbar-nav">
+          <li className="nav-item">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/resources">Resources</Link>
+          </li>
+          <li className="nav-item">
+            {this.authButton()}
+          </li>
+        </ul>
+      </nav>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,14 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import App from './components/app';
+import Resources from './components/Resources';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    <Router>
+      <div>
+        <Route path="/" component={App} />
+        <Route path="/resources" component={Resources} />
+      </div>
+    </Router>
+  </Provider>,
+  document.querySelector('.container')
+);
